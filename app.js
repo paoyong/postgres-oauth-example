17d7e26e2662401140f51f841d6702de56a6c133
Keith Yong <terda12@gmail.com>
2015-03-25 12:06:08 -0400

    Sign in and signup boilerplate code

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 var express = require('express'),
-    passport = require('passport'),
     Model = require('./model'),
     router = require('./routes/router.js'),
     app = express(),
     path = require('path'),
+    passport = require('passport'),
+    LocalStrategy = require('passport-local').Strategy;
     PORT = process.env.PORT || 1337;
+
+passport.use(new LocalStrategy(function(username, password, done) {
+    new Model.User({username: username}).fetch().then(function(data) {
+        var user = data;
+        if (user === null) {
+            return done(null, false, { message: 'Invalid username or password' });
+        } else {
+            user = data.toJSON();
+
+            if (!bcrypt.compareSync(password, user.password)) {
+                return done(null, false, { message: 'Invalid username or password' });
+            } else {
+                return done(null, user);
+            }
+        }
+    });
+}));
 
 var usernamePromise = new Model.User({username: 'keithy'}).fetch();
 
--- routes/router.js
+++ routes/router.js
@@ -1,15 +1,67 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
+
+// Used to encrypt user password before adding it to db.
+var bcrypt = require('bcrypt-nodejs');
+
+// Bookshelf postgres db ORM object. Basically it makes 
+// it simple and less error port to insert/query the db.
+var Model = require('../model.js');
 
 router.get('/', function(req, res, next) {
-    res.render('index');
+
+    // If user is not authenticated, redirect them
+    // to the signin page.
+    if (!req.isAuthenticated()) {
+        res.redirect('/signin');
+    } else {
+        res.render('index');
+    }
 });
 
 router.get('/signin', function(req, res, next) {
-    res.render('signin');
+    res.render('signin', { title: 'Sign In' });
 });
 
+// Add user to database.
 router.post('/signin', function(req, res, next) {
+    console.log('Signing in user!');
+
+    passport.authenticate('local', {
+                         successRedirect: '/',
+                         failureRedirect: '/signin'
+    }, function(err, user, info) {
+        if (err) {
+            return res.render('signin', { title: 'Sign In', errorMessage: err.message });
+        }
+
+        if (!user) {
+            return res.render('signin', { title: 'Sign In', errorMessage: info.message });
+        }
+
+        return req.logIn(user, function(err) {
+            if (err) {
+                return res.render('signin', { title: 'Sign In', errorMessage: err.message });
+            } else {
+                return res.redirect('/');
+            }
+        });
+    })(req, res, next);
+});
+
+router.get('/signup', function(req, res, next) {
+    if (req.isAuthenticated()) {
+        res.redirect('/');
+    } else {
+        res.render('signup', { title: 'Sign Up' });
+    }
+});
+
+router.post('/signup', function(req, res, next) {
+    console.log('Signup route');
+    console.log(req.body);
+    var user = req.body;
 });
 
 module.exports = router;
